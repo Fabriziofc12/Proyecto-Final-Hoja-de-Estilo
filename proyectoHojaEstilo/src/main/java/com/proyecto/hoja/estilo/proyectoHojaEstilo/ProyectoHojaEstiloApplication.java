af34c5fa220ac2cd32525b474fdb3a65f3640520
Salin Fabrizio Flores Condori <u23250409@utp.edu.pe>
2025-11-02 12:09:00 -0500

    casi

---
+++ proyectoHojaEstilo/src/main/java/com/proyecto/hoja/estilo/proyectoHojaEstilo/ProyectoHojaEstiloApplication.java
@@ -0,0 +1,11 @@
+package com.proyecto.hoja.estilo.proyectoHojaEstilo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoHojaEstiloApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoHojaEstiloApplication.class, args);
+    }
+}
