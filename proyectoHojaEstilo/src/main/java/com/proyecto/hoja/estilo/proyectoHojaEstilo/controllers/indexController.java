d5096f6dd71e33f641f837b4e7c8f0f3a1c25765
Salin Fabrizio Flores Condori <u23250409@utp.edu.pe>
2025-11-03 16:56:35 -0500

    oño

---
+++ proyectoHojaEstilo/src/main/java/com/proyecto/hoja/estilo/proyectoHojaEstilo/controllers/indexController.java
@@ -0,0 +1,19 @@
+package com.proyecto.hoja.estilo.proyectoHojaEstilo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@Controller
+public class indexController {
+    @GetMapping("/Index")
+    public String getIndex(Model model) {
+            model.addAttribute("titulo", "Página de Inicio");
+            model.addAttribute("header", "Logo");
+            model.addAttribute("contenido", "Landing Page");
+        return "Index";
+    }
+    
+}
